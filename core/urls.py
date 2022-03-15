72c1ffbb0ba187c14fd67b51b5d8f83bb0317b19
Yahyr Paredes <yahyrparedesarteaga@gmail.com>
2022-03-15 11:43:44 -0500

    fix

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework_swagger.views import get_swagger_view
+
+# SETTINGS SWAGGER
+schema_view = get_swagger_view(title='')
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Console Jobs API",
+      default_version='v1',
+      description="Console Jobs the best platform whit job for you!",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Django admin route
     path('api/company/', include("companies.urls")),  # UI Kits Html files
     path('api/postulant/', include("postulant.urls")),  # UI Kits Html files
-    path("", include("apps.home.urls"))  # UI Kits Html files
+    path("", include("apps.home.urls")),  # UI Kits Html files
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
