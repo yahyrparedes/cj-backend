767d88916367fd3ed9fbebcb0ff1e59e5ca1e6b5
Yahyr Paredes <yahyrparedesarteaga@gmail.com>
2022-03-16 23:35:08 -0500

    update postulant

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from .models import WorkDay, WorkModality, WorkExperience, WorkArea, JobRole
+
+from postulant.models import Postulant
+from postulant.serializer import PostulantSerializer
+from .models import WorkDay, WorkModality, WorkExperience, WorkArea, JobRole, Postulate, Job
 
 
 class WorkDaySerializer(serializers.ModelSerializer):
@@ -40,3 +43,38 @@ class JobRoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobRole
         fields = '__all__'
+
+
+class JobSerializer(serializers.ModelSerializer):
+    pass
+
+
+class PostulateToJobRegisterSerializer(serializers.ModelSerializer):
+    job = serializers.IntegerField(required=True)
+    postulant = serializers.IntegerField(required=True)
+
+    class Meta:
+        model = Postulate
+        fields = ('job', 'postulant')
+
+
+class PostulateToJobSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    job = JobSerializer()
+    postulant = PostulantSerializer()
+
+    def validate_job(self, value):
+        if not Job.objects.filter(pk=value).exists():
+            raise serializers.ValidationError("Job don't exist!")
+
+    def validate_postulant(self, value):
+        if not Postulant.objects.filter(pk=value).exists():
+            raise serializers.ValidationError("Postulant don't exist!")
+
+    def save(self, **kwargs):
+        postulant = Postulant.objects.create(**kwargs)
+        return postulant
+
+    class Meta:
+        model = Postulate
+        fields = ('id', 'job', 'postulant', 'is_active')
